26f2fbb7f9ea20ff9c693ea536d017ea6c541e57
song.cpe <song.cpe@gmail.com>
2023-01-04 15:01:29 +0700

    Injection usecase instead service

--- src/main/kotlin/com/example/demo/app/rest/user/UserController.kt
+++ src/main/kotlin/com/example/demo/app/rest/user/UserController.kt
@@ -2,6 +2,7 @@ package com.example.demo.app.rest.user
 
 import com.example.demo.app.rest.user.model.UserResponse
 import com.example.demo.domain.service.user.GetUserCommand
+import com.example.demo.domain.service.user.GetUserUseCase
 import com.example.demo.domain.service.user.UserService
 import com.example.demo.domain.service.user.model.GetUserResult
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/users")
-class UserController(private val userService: UserService) {
+class UserController(private val getUserUseCase: GetUserUseCase) {
 
     @GetMapping("/{username}")
     fun getUser(@PathVariable username: String): UserResponse {
         val command = GetUserCommand(username = username)
-        val result = userService.getUser(command)
+        val result = getUserUseCase.getUser(command)
         return toGetUserResponse(result)
     }
 
